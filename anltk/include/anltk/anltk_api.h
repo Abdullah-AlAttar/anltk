09cabc3117f0c9c6e3f185f70aeee2d25006946c
abdullah.alattar <abdullah.mohammad.alattar@gmail.com>
2021-06-15 21:15:40 +0400

    udpated api docs

--- anltk/include/anltk/anltk_api.h
+++ anltk/include/anltk/anltk_api.h
@@ -89,15 +89,49 @@ ANLTK_PUBLIC const char* anltk_mofaqqet_tafqeet(Mofaqqet*, long long input);
  */
 ANLTK_PUBLIC bool anltk_is_valid_kalima(const char* input);
 
+/**
+ * @brief Check wether a character is a valid tashkeel from the following list 
+ * { TANWEEN_FATHA, TANWEEN_DAMMA, TANWEEN_KASRA, FATHA, DAMMA, KASRA, SHADDA, SUKUN }
+ * 
+ * @param input : should be a single arabic character.
+ * @return ANLTK_PUBLIC 
+ */
+
 ANLTK_PUBLIC bool anltk_is_tashkeel(const char* input);
 
+/**
+ * @brief Checks wether a character is from the alphabet
+ * 
+ * @param input : should be a single arabic character. will return false if empty or more than 1
+ * @return ANLTK_PUBLIC 
+ */
 ANLTK_PUBLIC bool anltk_is_arabic_alpha(const char* input);
-
+/**
+ * @brief creates preprocessor
+ * 
+ * @return ANLTK_PUBLIC* 
+ */
 ANLTK_PUBLIC Preprocessor* anltk_preprocessor_new();
 
+/**
+ * @brief Removes all tashkeel from the given arabic text, 
+ * the tashkeel list is { TANWEEN_FATHA, TANWEEN_DAMMA, TANWEEN_KASRA, FATHA, DAMMA, KASRA, SHADDA, SUKUN }
+ * @param input 
+ * @return ANLTK_PUBLIC const* 
+ */
 ANLTK_PUBLIC const char* anltk_preprocessor_remove_tashkeel(Preprocessor*, const char* input);
+/**
+ * @brief Removes small tashkeel from the given arabic text
+ * the small list is { SMALL_ALEF '\u0670',  SMALL_WAW : '\u06E5', SMALL_YEH : '\u06E6' }
+ * @param input 
+ * @return ANLTK_PUBLIC const* 
+ */
 ANLTK_PUBLIC const char* anltk_preprocessor_remove_small(Preprocessor*, const char* input);
-
+/**
+ * @brief Releases the preprocessor
+ * 
+ * @return ANLTK_PUBLIC 
+ */
 ANLTK_PUBLIC void anltk_preprocessor_free(Preprocessor*);
 
 #ifdef __cplusplus
