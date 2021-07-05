3f3df339d82adcef6ac54e4892fb7321a22e457d
abdullah.alattar <abdullah.mohammad.alattar@gmail.com>
2021-07-05 21:15:46 +0400

    Added memory mapping files, maybe it's gonna be used later

--- anltk/tests/dummy.cpp
+++ anltk/tests/dummy.cpp
@@ -3,6 +3,7 @@
 #include <algorithm>
 #include <iostream>
 
+#include <MemoryMapped.h>
 #include <algorithm>
 #include <chrono>
 #include <fstream>
@@ -17,7 +18,7 @@ auto timeFuncInvocation = [](auto&& func, auto&&... params) {
     std::forward<decltype(func)>(func)(std::forward<decltype(params)>(params)...);
     // get time after function invocation
     const auto& stop = std::chrono::high_resolution_clock::now();
-return std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
+    return std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count();
 };
 
 int main(int argc, char** argv)
@@ -25,16 +26,25 @@ int main(int argc, char** argv)
     std::string file_name = argv[1];
     std::ifstream t(file_name.c_str());
 
-    std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+    // std::string str((std::istreambuf_iterator<char>(t)), std::istreambuf_iterator<char>());
+    // MemoryMapped data(file_name.c_str());
+    // const char* str = (const char*) data.getData();
 
     std::ofstream out_file;
     out_file.open("utfcpp.txt");
-    std::string output;
+    std::string line;
     ANLTK_Preprocessor* pp = anltk_preprocessor_new();
-    std::cout << timeFuncInvocation([&]() {
-       output = anltk_preprocessor_remove_tashkeel(pp, str.c_str());
-    }) << " s" << std::endl;
-    out_file << output;
+
+    while (std::getline(t, line))
+    {
+        // line contains the current line
+        std::string output;
+        timeFuncInvocation([&]() {
+            output = anltk_preprocessor_remove_tashkeel(pp, line.c_str());
+        });
+        out_file << output << '\n';
+    }
+
     anltk_preprocessor_free(pp);
 
     // مرحبا  بكم�!
