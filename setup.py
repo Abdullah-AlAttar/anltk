ab1cb4e20810bc995e66673d51191336520e63cd
abdullah.alattar <abdullah.mohammad.alattar@gmail.com>
2021-10-20 13:26:02 +0400

    refactor setup.py

--- setup.py
+++ setup.py
@@ -5,28 +5,28 @@ import pybind11
 from pybind11.setup_helpers import Pybind11Extension, build_ext
 
 # Figure out environment for cross-compile
-anltk_source = os.getenv(
-    "ANLTK_SOURCE", os.path.abspath(os.path.dirname(__file__)))
+anltk_source = os.getenv("ANLTK_SOURCE",
+                         os.path.abspath(os.path.dirname(__file__)))
 system = os.environ.get('ANLTK_PLATFORM', platform.system())
-
 
 with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
 
 
 def _get_project_version():
-    version_path = os.path.join(
-        anltk_source, "python", "anltk_module", "anltk", "version.py")
+    version_path = os.path.join(anltk_source, "python", "anltk_module",
+                                "anltk", "version.py")
     version = {}
     with open(version_path, encoding="utf-8") as fp:
         exec(fp.read(), version)
     return version["__version__"]
 
 
-include_dirs = [pybind11.get_include(),
-                os.path.join(anltk_source, 'anltk', 'include'),
-                os.path.join(anltk_source, 'subprojects', 'utfcpp', 'source')
-                ]
+include_dirs = [
+    pybind11.get_include(),
+    os.path.join(anltk_source, 'anltk', 'include'),
+    os.path.join(anltk_source, 'subprojects', 'utfcpp', 'source')
+]
 
 cflags = ['-O3']
 ldflags = []
@@ -34,26 +34,25 @@ ldflags = []
 if system == 'Windows':
     cflags = ['/O2', '/utf-8']
 
-anltk_src_files = map(str, os.listdir(
-    os.path.join(anltk_source, 'anltk', 'src')))
+anltk_src_files = map(str,
+                      os.listdir(os.path.join(anltk_source, 'anltk', 'src')))
 anltk_src_cpp = list(filter(lambda x: x.endswith('.cpp'), anltk_src_files))
 anltk_src_cpp.remove('unicode_normalization.cpp')
 anltk_src_cpp = list(
-    map(lambda x: str(os.path.join(anltk_source, 'anltk', 'src', x)), anltk_src_cpp)
-)
+    map(lambda x: str(os.path.join(anltk_source, 'anltk', 'src', x)),
+        anltk_src_cpp))
 
-anltk_pybind_module_cpp = os.path.join(
-                          anltk_source, 'python', 'anltk_module', 'anltk',
-                          'pybind', 'anltk_pybind.cpp')
+anltk_pybind_module_cpp = os.path.join(anltk_source, 'python', 'anltk_module',
+                                       'anltk', 'pybind', 'anltk_pybind.cpp')
 ext_modules = [
     Pybind11Extension("anltk_pybind",
-                      sources= anltk_src_cpp + [anltk_pybind_module_cpp],
+                      sources=anltk_src_cpp + [anltk_pybind_module_cpp],
                       extra_compile_args=cflags,
                       cxx_std=17,
                       extra_link_args=ldflags,
                       include_dirs=include_dirs,
-                      define_macros=[("UTF_CPP_CPLUSPLUS", "201703L"), ("BUILDING_PYBIND11", "1")]
-                      )
+                      define_macros=[("UTF_CPP_CPLUSPLUS", "201703L"),
+                                     ("BUILDING_PYBIND11", "1")])
 ]
 
 setuptools.setup(
@@ -74,13 +73,11 @@ setuptools.setup(
     zip_safe=False,
     include_package_data=False,
     package_dir={str(''): str('python/anltk_module')},
-    packages=[
-        str('anltk'),
-        str('anltk.constants'),
-        str('anltk.tests')
-    ],
+    packages=[str('anltk'),
+              str('anltk.constants'),
+              str('anltk.tests')],
     classifiers=[
-        'Development Status :: 1 - Beta',
+        'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 3',
@@ -93,5 +90,4 @@ setuptools.setup(
         'Operating System :: Unix',
         'Operating System :: MacOS',
     ],
-    keywords = ['NLP', 'Arabic', 'python', 'arabic', 'c++']
-)
+    keywords=['NLP', 'Arabic', 'python', 'arabic', 'c++'])
